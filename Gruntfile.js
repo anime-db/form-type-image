14984e07cabfb4fda68612dc21a4944a9bbaf6f3
Peter Gribanov <wcode404@gmail.com>
2016-04-20 17:21:22 +0300

    Correct path in grunt config. Add filedrop to jsFiles

--- Gruntfile.js
+++ Gruntfile.js
@@ -8,11 +8,11 @@ module.exports = function(grunt) {
             js: 'src/Resources/public/js/',
             jsSrc: 'src/Resources/public/js/src/',
             scss: 'src/Resources/public/scss/',
-            filedrop: '<%= project.js %>/vendor/weixiyen/jquery-filedrop/jquery.filedrop.js',
             jsFiles: [
-                '<%= project.jsSrc %>/common.js',
-                '<%= project.jsSrc %>/image.js',
-                '<%= project.jsSrc %>/image_collection.js'
+                '<%= project.js %>vendor/weixiyen/jquery-filedrop/jquery.filedrop.js',
+                '<%= project.jsSrc %>common.js',
+                '<%= project.jsSrc %>image.js',
+                '<%= project.jsSrc %>image_collection.js'
             ]
         },
         sass: {
@@ -34,7 +34,7 @@ module.exports = function(grunt) {
         },
         concat: {
             js: {
-                src: ['<%= project.filedrop %>', '<%= project.jsFiles %>'],
+                src: '<%= project.jsFiles %>',
                 dest: '<%= project.js %>upload_image.js'
             }
         },
@@ -50,17 +50,16 @@ module.exports = function(grunt) {
                     mangle: false // do not change the names of the functions in the minification
                 },
                 files: {
-                    '<%= project.js %>/upload_image.min.js': '<%= project.js %>/upload_image.js'
+                    '<%= project.js %>upload_image.min.js': '<%= project.js %>upload_image.js'
                 }
             }
         }
     });
 
     grunt.loadNpmTasks('grunt-contrib-sass');
-    grunt.loadNpmTasks('grunt-contrib-uglify');
     grunt.loadNpmTasks('grunt-contrib-cssmin');
     grunt.loadNpmTasks('grunt-contrib-concat');
-
+    grunt.loadNpmTasks('grunt-contrib-uglify');
 
     grunt.registerTask('default', ['sass', 'cssmin', 'concat', 'uglify']);
 };
