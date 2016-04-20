c2fc527ed7fa6265bddc1e719be85bda636c6324
Peter Gribanov <wcode404@gmail.com>
2016-04-20 10:43:34 +0300

    add bundle class

---
+++ src/AnimeDbFormTypeImageBundle.php
@@ -0,0 +1,21 @@
+<?php
+/**
+ * AnimeDb package
+ *
+ * @package   AnimeDb
+ * @author    Peter Gribanov <info@peter-gribanov.ru>
+ * @copyright Copyright (c) 2016, Peter Gribanov
+ * @license   http://opensource.org/licenses/MIT
+ */
+
+namespace AnimeDb\Bundle\FormTypeImageBundle;
+
+use Symfony\Component\HttpKernel\Bundle\Bundle;
+
+/**
+ * AnimeDbFormTypeImageBundle
+ * @package AnimeDb\Bundle\FormTypeImageBundle
+ */
+class AnimeDbFormTypeImageBundle extends Bundle
+{
+}
